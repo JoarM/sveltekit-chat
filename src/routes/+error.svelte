c62214e6e28b344fede487844adc7900cfde4cdc
JoarM <joar.maltesson@gmail.com>
2023-04-17 20:05:08 +0200

    depolyed

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Navbar from "../components/UI/navbar.svelte";
+
+</script>
+<Navbar></Navbar>
+<div class="container m-auto">
+    <div class="max-w-3xl mx-auto flex flex-col items-center">
+        <h1 class="text-5xl mt-12 font-sans font-bold">404 NOT FOUND</h1>
+        <img src="/home.gif" alt="Time to go home." class="m-12 w-3/5">
+        <a href="/" class="bg-white text-black px-4 py-2 font-bold">GO HOME</a>
+    </div>
+</div>
