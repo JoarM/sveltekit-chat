e95e09ca880ec809d29c0dbd5ede59c390bd8fe1
JoarM <joar.maltesson@gmail.com>
2023-04-18 21:46:17 +0200

    working on input

--- src/components/messages/input.svelte
+++ src/components/messages/input.svelte
@@ -11,7 +11,8 @@
         if (!user) return;
         if (!user.displayName) return;
         if (!user.photoURL) return;
-        if (message.innerText === "") return;
+        if (message.innerText.trim() === "") return;
+        console.log(message.innerText)
         const { collection, addDoc, getFirestore, Timestamp } = await import("firebase/firestore");
         const firestore = getFirestore();
         const messageRef = collection(firestore, "messages");
@@ -30,18 +31,21 @@
     if (browser) {
         window.addEventListener('keydown',(e: KeyboardEvent) => {
             if (e.key === "Shift") shift = true;
-
-            if (e.key === "Enter" && !shift) sendMessage();
         });
 
         window.addEventListener('keyup', (e: KeyboardEvent) => {
             if (e.key === "Shift") shift = false;
         });
     }
+
+    function keyboardTyping(e: any) {
+        if (e.key === "Enter" && !shift) sendMessage();
+    }
 </script>
 
-<form class="flex items-center gap-1 mx-3" on:submit={sendMessage}>
-    <span bind:this={message} placeholder="Message chatroom" class="p-2 rounded-md flex-grow outline-none border-none text-slate-200 bg-slate-800" contenteditable="true"></span>
+<form class="flex items-center gap-1 mx-3"  on:submit={sendMessage}>
+    <!-- svelte-ignore a11y-interactive-supports-focus -->
+    <span bind:this={message} contenteditable="true" role="textbox" spellcheck="true" on:keypress={keyboardTyping}></span>
     <button class="bg-slate-800 p-2 rounded-md text-slate-200 sm:hidden">
         <svg viewBox="0 0 512 512"><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
     </button>
@@ -55,7 +59,12 @@
     }
 
     span {
+        @apply p-2 rounded-md flex-grow outline-none border-none text-slate-200 bg-slate-800;
         width: 100%;
+        overflow-wrap: break-word;
+        position: relative; 
+        outline: none;
+        white-space: pre-wrap;
         overflow-wrap: break-word;
     }
 </style>
